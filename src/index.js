55f2968a379a63c23b19e282ba507b4945428e56
Anton Semenikhin <protest_the_hero@mail.ru>
2022-02-22 22:33:55 +0300

    in process

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import timerModule from "./modules/timer";
+
+timerModule('20  febuary 2022'); // вызов функции. Передаем дату deadline
---
+++ src/modules/two.js
@@ -0,0 +1,5 @@
+const twoModule = () => {
+
+};
+
+export default twoModule;
