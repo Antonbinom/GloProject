a05ea37432aa152939f6585a0136c3214dcf2f65
Anton Semenikhin <protest_the_hero@mail.ru>
2022-02-23 14:22:45 +0300

    Lesson 19 main - ready

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -26,27 +26,32 @@ const timerModule = (deadline) => {
 
 	// функция вывода дэдлайна на страницу
 	const updateClock = () => {
-
 		let getTime = getTimeRemeining(); // запускаем расчеты
 
-
 		// проверка
-		if (getTime.days % 10 > 1 && getTime.days % 10 < 5) timerDays.textContent = getTime.days + ' дня /';
-		else if (days % 10 == 1 && getTime.days != 11) timerDays.textContent = getTime.days + ' день /';
+		if (5 > getTime.days % 10 > 1) timerDays.textContent = getTime.days + ' дня /';
+		else if (getTime.days % 10 == 1 && getTime.days != 11) timerDays.textContent = getTime.days + ' день /';
 		else timerDays.textContent = getTime.days + ' дней /';
 
-		if (getTime.hours >= 0 && getTime.hours < 10) timerHours.textContent = '0' + getTime.hours + ' ч';
+		if (10 > getTime.hours >= 0) timerHours.textContent = '0' + getTime.hours + ' ч';
 		else timerHours.textContent = getTime.hours + ' ч';
-		if (getTime.minutes >= 0 && getTime.minutes < 10) timerMinutes.textContent = '0' + getTime.minutes + ' м';
+		if (10 > getTime.minutes >= 0) timerMinutes.textContent = '0' + getTime.minutes + ' м';
 		else timerMinutes.textContent = getTime.minutes + ' м';
-		if (getTime.seconds >= 0 && getTime.seconds < 10) timerSeconds.textContent = '0' + getTime.seconds + ' с';
+		if (10 > getTime.seconds >= 0) timerSeconds.textContent = '0' + getTime.seconds + ' с';
 		else timerSeconds.textContent = getTime.seconds + ' с';
-
-		// проверка на отрицательное значение
-		if (getTime.timeRemaining > 0) setTimeout(updateClock, 1000);
 	};
 
-	updateClock();
+	// вывод времени каждую секунду
+	let getTime = getTimeRemeining(); // запускаем расчеты
+	let idInterval = setInterval(() => {
+		if (getTime.timeRemaining > 0) { // выводим пока таймер > 0
+			updateClock();
+		} else {
+			clearTimeout(idInterval);
+			console.log('Остановка!');
+		}
+	}, 1000);
+	// updateClock();
 };
 
 export default timerModule;
