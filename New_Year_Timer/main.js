26d9fe231e8a54ef6a1119233de9f90faad8c1bc
Anton Semenikhin <protest_the_hero@mail.ru>
2022-02-25 00:23:23 +0300

    newYeartimer fix

--- New_Year_Timer/main.js
+++ New_Year_Timer/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const hello = document.querySelector('.hello');
+const todayText = document.querySelector('.today');
+const time = document.querySelector('.time');
+const timer = document.querySelector('.timer');
+let helloText;
+
 let date = new Date();
 let hours = date.getHours();
 let minutes = date.getMinutes();
@@ -12,19 +18,20 @@ let today = new Intl.DateTimeFormat('ru-RU', options).format(date);
 
 //Добрый день(утро, вечер, ночь в зависимости от времени суток)
 const setTime = (date) => {
-	if (date < 9 && date > 4) console.log('Доброе утро');
-	else if (date < 18 && date > 12) console.log('Добрый день');
-	else if (date < 23 && date > 18) console.log('Добрый вечер');
-	else console.log('Доброй ночи');
-}
+	if (date < 9 && date > 4) helloText = 'Доброе утро';
+	else if (date < 18 && date > 12) helloText = 'Добрый день';
+	else if (date < 23 && date > 18) helloText = 'Добрый вечер';
+	else helloText = 'Доброй ночи';
+	hello.textContent = helloText;
+};
 setTime(date.getHours());
 
 // Сегодня: Понедельник
-console.log('Сегодня: ' + today);
+todayText.textContent = 'Сегодня: ' + today;
 
 // выоводит дату точно в таком виде Текущее время: 12:05:15 PM
-if (hours < 12 && hours >= 0) console.log('Текущее время: ' + date.toLocaleTimeString() + ' AM');
-else console.log('Текущее время: ' + date.toLocaleTimeString() + ' PM');
+if (hours < 12 && hours >= 0) time.textContent = 'Текущее время: ' + date.toLocaleTimeString() + ' AM';
+else time.textContent = 'Текущее время: ' + date.toLocaleTimeString() + ' PM';
 
 const newYearTimer = (dayX) => {
 	let dayNow = new Date().getTime();
@@ -33,6 +40,6 @@ const newYearTimer = (dayX) => {
 	// До нового года осталось 312 дней
 	let timeRemaining = (newYear - dayNow) / 1000;
 	let days = Math.ceil(timeRemaining / 60 / 60 / 24);
-	console.log(days);
+	timer.textContent = 'До нового года осталось ' + days + ' дней';
 };
 newYearTimer('01 January 2023');
