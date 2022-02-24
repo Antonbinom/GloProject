be28e825d17db00fad85766d241273f4a210aa51
Anton Semenikhin <protest_the_hero@mail.ru>
2022-02-24 11:12:48 +0300

    lesson19 is ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import timerModule from "./modules/timer";
 
-timerModule('25 febuary 2022'); // вызов функции. Передаем дату deadline
+timerModule('25 march 2022'); // вызов функции. Передаем дату deadline
