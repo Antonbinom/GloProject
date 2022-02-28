f386b17613a6df1d555c5b569071b6b9e702b9e7
Anton Semenikhin <protest_the_hero@mail.ru>
2022-02-28 15:20:06 +0300

    build

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -5,6 +5,7 @@ const validationModule = () => {
 	const inputText = document.querySelectorAll('[name="user_message"], [name="user_name"]');
 	const inputPhone = document.querySelectorAll('[type="tel"]');
 	const inputEmail = document.querySelectorAll('[type="email"]');
+	const userFormInputs = document.querySelectorAll('[name="user_form"] input');
 
 	calcInputs.forEach((item) => {
 		item.addEventListener('input', (e) => {
@@ -17,8 +18,8 @@ const validationModule = () => {
 			e.target.value = e.target.value.replace(/[^а-я\ \-\.\,]/gi, "");
 		});
 		item.addEventListener('blur', (e) => {
-			e.target.value = e.target.value.replace(/(^[\s\-]{0,})([\ \-])+/g, (str, $1, $2) => {
-				return $2;
+			e.target.value = e.target.value.replace(/([\-\ ]|^)[а-яё]/g, function (u) {
+				return u.toUpperCase();
 			});
 		});
 	});
@@ -34,6 +35,13 @@ const validationModule = () => {
 		});
 	});
 
+	userFormInputs.forEach((item) => {
+		item.addEventListener('blur', (e) => {
+			e.target.value = e.target.value.replace(/^\-+|\-+$/g, '').replace(/([\s\-])+/g, (str, $1) => {
+				return $1;
+			});
+		});
+	});
 };
 
 export default validationModule;
